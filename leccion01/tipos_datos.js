4a94345ecbca7b185fee6f4b25fb428aac21d069
SiAn20 <sidneyangellysg@gmail.com>
2024-12-30 16:48:02 -0400

    feat: data types and how to see their type

---
+++ leccion01/tipos_datos.js
@@ -0,0 +1,52 @@
+//Tipos de Datos
+    var nombre = "Sidney Sahonero Garrado"; //String
+    console.log(nombre);
+
+    var numero = 2005; //numerico
+    console.log(numero);
+
+    var object = { //objeto
+        name: "Sidney",
+        apellido: "Sahonero",
+        edad: 19
+    };
+    var objeto2 = null;
+    console.log(object);
+    console.log(typeof objeto2);
+
+    var verdad = true; //booleano
+    console.log(verdad);
+
+    function mifuncion(){} //function
+    console.log(mifuncion);
+    class persona{
+        constructor(nombre, apellido){
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+    }
+    console.log(typeof persona);
+
+    var x; //undefined
+    console.log(typeof x);
+
+
+//saber los tipos de datos
+    var tipo = "primero cadena";
+    console.log(typeof tipo);
+    tipo = 20012005;
+    console.log(typeof tipo);
+
+
+//Arrays
+    var pelicula = ['goblin','cd'];
+    console.log(pelicula);
+    console.log(typeof pelicula);//object
+
+
+//Uso de lef const
+    let sid = "variable"
+    console.log(sid);
+
+    const ang = "no puede cambiar o provocara un error";
+    console.log(ang);
